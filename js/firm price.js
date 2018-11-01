7f9e6a2aa8d8c44f02577a4a8e8d579f49852341
Qiong0 <614907275@qq.com>
2018-11-01 23:08:04 +0800

    food

--- js/firm price.js
+++ js/firm price.js
@@ -2,13 +2,19 @@
 * @Author: Lenovo
 * @Date:   2018-09-14 17:46:05
 * @Last Modified by:   Lenovo
-* @Last Modified time: 2018-09-14 17:51:58
+* @Last Modified time: 2018-09-18 19:02:09
 */
 window.onload=function(){
 	let btn=document.querySelector("footer .end");
 	let cover=document.querySelector(".coverbox");
-	// console.log(btn);
+	let jian=document.querySelectorAll(".box .jian");
+	let add=document.querySelectorAll(".box .add")[2];
+	// console.log(jia,jian);
 	btn.onclick=function(){
 		cover.style.display="block";
+	}
+
+	add.onclick=function(){
+		jian.style.display="block";
 	}
 }
